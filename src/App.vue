2d89e64cc6238142add7705b6f48911e326f47b8
ling <971039649@qq.com>
2022-01-22 14:43:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-config-provider :locale="zhCN">
+    <router-view />
+  </a-config-provider>
+</template>
+
+<script lang="ts" setup>
+import zhCN from 'ant-design-vue/es/locale/en_US'
+import { useTitle } from '/@/hooks/useTitle'
+import dayjs from 'dayjs'
+import 'dayjs/locale/en'
+
+dayjs.locale('en')
+
+useTitle()
+
+console.log('my config env: ', import.meta.env)
+</script>
